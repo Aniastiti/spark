483d9dfe6c27ea9262c941e25e9059666ce374a3
Aniastiti <129522733+Aniastiti@users.noreply.github.com>
2023-07-09 23:40:34 +0200

    Update E04_with_column.scala
    Membres du groupe: 
    Ania Stiti
    Nihel Chaabi
    Amrane Younis
    Amalou Kamilia

--- src/main/scala/com/fabulouslab/spark/e4_stream/E04_with_column.scala
+++ src/main/scala/com/fabulouslab/spark/e4_stream/E04_with_column.scala
@@ -1,7 +1,7 @@
 package com.fabulouslab.spark.e04_stream
 
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.functions.{current_date, from_json}
+import org.apache.spark.sql.functions.{current_date, from_json, col, expr}
 import org.apache.spark.sql.types.{LongType, StringType, StructField, StructType}
 
 object E04_with_column {
@@ -17,7 +17,44 @@ object E04_with_column {
       *      - Le temps passé sur la page en second
       * */
 
+    // Créer une session Spark
+    val spark = SparkSession.builder()
+      .appName("Enriching Stream")
+      .master("local[2]")
+      .getOrCreate()
 
+    // Définir le schéma pour la structure JSON
+    val jsonSchema = StructType(Seq(
+      StructField("viewtime", LongType, nullable = true),
+      StructField("userid", StringType, nullable = true),
+      StructField("pageid", StringType, nullable = true)
+    ))
+
+    // Lire le flux Kafka en tant que DataFrame de chaînes de caractères
+    val kafkaDF = spark.readStream
+      .format("kafka")
+      .option("kafka.bootstrap.servers", "localhost:9092")
+      .option("subscribe", "pageviews")
+      .load()
+
+    // Convertir la colonne "value" du DataFrame en JSON
+    val jsonDF = kafkaDF.selectExpr("CAST(value AS STRING)")
+      .select(from_json(col("value"), jsonSchema).as("data"))
+      .select("data.*")
+
+    // Ajouter une colonne pour la date de traitement de l'événement
+    val enrichedDF = jsonDF.withColumn("processing_date", current_date())
+
+    // Ajouter une colonne pour le temps passé sur la page en secondes
+    val enrichedDFWithTime = enrichedDF.withColumn("time_spent_seconds", expr("viewtime / 1000"))
+
+    // Afficher le DataFrame résultant
+    val query = enrichedDFWithTime.writeStream
+      .format("console")
+      .outputMode("append")
+      .start()
+
+    query.awaitTermination()
   }
 
 }
