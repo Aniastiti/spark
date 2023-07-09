05918348c2e25e8421b75409da97ed919f59f79a
Aniastiti <129522733+Aniastiti@users.noreply.github.com>
2023-07-09 23:32:55 +0200

    Update E02_json.scala
    membres groupe: 
    Ania stiti 
    Nihel Chaabi
    Amalou Kamilia
    Amrane Younis

--- src/main/scala/com/fabulouslab/spark/e4_stream/E02_json.scala
+++ src/main/scala/com/fabulouslab/spark/e4_stream/E02_json.scala
@@ -11,10 +11,43 @@ object E02_json {
 
     /**
       *   Toujours sur le flux du topic pageviews :
-      *   - Déserialiser la value json en Struct
-      *   - Construire un dataframe depuis les attributs du Struct
+      *   - Déserialiser la valeur JSON en Struct
+      *   - Construire un dataframe à partir des attributs du Struct
       * */
 
+    // Créer une session Spark
+    val spark = SparkSession.builder()
+      .appName("JSON Processing")
+      .master("local[2]")
+      .getOrCreate()
+
+    // Définir le schéma de la structure JSON
+    val jsonSchema = StructType(Seq(
+      StructField("timestamp", LongType, nullable = true),
+      StructField("user", StringType, nullable = true),
+      StructField("page", StringType, nullable = true),
+      StructField("click", BooleanType, nullable = true)
+    ))
+
+    // Lire le flux Kafka en tant que DataFrame de strings
+    val kafkaDF = spark.readStream
+      .format("kafka")
+      .option("kafka.bootstrap.servers", "localhost:9092")
+      .option("subscribe", "pageviews")
+      .load()
+
+    // Convertir la colonne "value" du DataFrame en JSON
+    val jsonDF = kafkaDF.selectExpr("CAST(value AS STRING)")
+      .select(from_json($"value", jsonSchema).as("data"))
+      .select("data.*")
+
+    // Afficher le DataFrame résultant
+    val query = jsonDF.writeStream
+      .format("console")
+      .outputMode("append")
+      .start()
+
+    query.awaitTermination()
   }
 
 }
