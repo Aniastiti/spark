d385a051152d412b7e2da2276dc79511dcde7f19
Aniastiti <129522733+Aniastiti@users.noreply.github.com>
2023-07-09 23:42:11 +0200

    Update E05_agg.scala

--- src/main/scala/com/fabulouslab/spark/e4_stream/E05_agg.scala
+++ src/main/scala/com/fabulouslab/spark/e4_stream/E05_agg.scala
@@ -18,6 +18,59 @@ object E05_agg {
       *   - Compter en une seule fois le temps moyen et max passé sur une page.
       * */
 
+    // Créer une session Spark
+    val spark = SparkSession.builder()
+      .appName("Aggregating Stream")
+      .master("local[2]")
+      .getOrCreate()
+
+    // Définir le schéma pour la structure JSON
+    val jsonSchema = StructType(Seq(
+      StructField("viewtime", LongType, nullable = true),
+      StructField("userid", StringType, nullable = true),
+      StructField("pageid", StringType, nullable = true)
+    ))
+
+    // Lire le flux Kafka en tant que DataFrame de chaînes de caractères
+    val kafkaDF = spark.readStream
+      .format("kafka")
+      .option("kafka.bootstrap.servers", "localhost:9092")
+      .option("subscribe", "pageviews")
+      .load()
+
+    // Convertir la colonne "value" du DataFrame en JSON
+    val jsonDF = kafkaDF.selectExpr("CAST(value AS STRING)")
+      .select(from_json(col("value"), jsonSchema).as("data"))
+      .select("data.*")
+
+    // Calculer le temps moyen passé sur une page
+    val avgTimeDF = jsonDF.selectExpr("avg(viewtime) as avg_time_spent")
+
+    // Calculer le temps max passé sur une page
+    val maxTimeDF = jsonDF.selectExpr("max(viewtime) as max_time_spent")
+
+    // Calculer le temps moyen et max passé sur une page en une seule fois
+    val avgMaxTimeDF = jsonDF.selectExpr("avg(viewtime) as avg_time_spent", "max(viewtime) as max_time_spent")
+
+    // Afficher les résultats
+    val query1 = avgTimeDF.writeStream
+      .format("console")
+      .outputMode("complete")
+      .start()
+
+    val query2 = maxTimeDF.writeStream
+      .format("console")
+      .outputMode("complete")
+      .start()
+
+    val query3 = avgMaxTimeDF.writeStream
+      .format("console")
+      .outputMode("complete")
+      .start()
+
+    query1.awaitTermination()
+    query2.awaitTermination()
+    query3.awaitTermination()
   }
 
 }
